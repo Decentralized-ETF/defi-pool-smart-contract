b0996ab587a781d740e3d0b288700ecc0ed6714d
ahmad mkh <apolonsoft@gmail.com>
2022-02-11 14:45:33 +0330

    new tests for front

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import Web3Modal from "web3modal";
 import { ethers } from "ethers";
-import TestInvestment from "../../build/contracts/TestInvestment.json";
+import Pool from "../../artifacts/contracts/Pool.sol/Pool.json";
 import Erc20Token from "../abi/Erc20Token.json";
 
-const contractAddress = '0xeDe20d657E4472EfEBC7033cBa87f79a0E1926ba';
-const tokenAddress = '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889';
+const contractAddress = '0xBF2ab16E23ed3C088aaB82Efef6cDC55eFC8CE64';
+const tokenAddress = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
 function HomePage() {
 
     const invest = async (e) => {
@@ -15,9 +15,9 @@ function HomePage() {
             const accounts = await provider.listAccounts();
             const signer = provider.getSigner();
             const contract = new ethers.Contract(contractAddress,
-                TestInvestment.abi, signer);
+                Pool.abi, signer);
 
-
+/*
             const result1 = await contract.getPoolTokens(
                 {
                     gasLimit: 3500000,
@@ -29,7 +29,7 @@ function HomePage() {
                     gasLimit: 3500000,
                 });
             console.log(result2);
-
+*/
             const value = ethers.utils.parseUnits('0.01', 'ether')
             console.log(value.toString());
 
@@ -45,6 +45,7 @@ function HomePage() {
 
 
             const tx = await contract.initInvestment(
+                accounts[0],value,
                 {
                     from: accounts[0],
                     value,
@@ -58,100 +59,6 @@ function HomePage() {
         }
     }
 
-    const investLocal = async (e) => {
-        try {
-            // const web3Modal = new Web3Modal();
-            //const connection = await web3Modal.connect();
-            const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
-            const accounts = await provider.listAccounts();
-            console.log(accounts);
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(contractAddress,
-                TestInvestment.abi, signer);
-            const value = ethers.utils.parseUnits('5', 'ether')
-            console.log(value.toString());
-
-            const tx = await contract.initInvestment(
-                {
-                    value,
-                    gasLimit: 3500000,
-                });
-            console.log(tx);
-            await tx.wait();
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-    const getMyInvestmentsLocal = async (e) => {
-        try {
-            // const web3Modal = new Web3Modal();
-            //const connection = await web3Modal.connect();
-            const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(contractAddress,
-                TestInvestment.abi, signer);
-            const result = await contract.getMyInvestments(
-                {
-                    gasLimit: 3500000,
-                });
-            console.log(result);
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-    const getInvestDataLocal = async (e) => {
-        try {
-            const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(contractAddress,
-                TestInvestment.abi, signer);
-            const result = await contract.getMyInvestment(0,
-                {
-                    gasLimit: 3500000,
-                });
-            console.log(result);
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-    const rebalanceLocal = async (e) => {
-        try {
-            const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(contractAddress,
-                TestInvestment.abi, signer);
-            const result = await contract.rebalance('0x5A22204Da599a931f0F8d5bC25D56f91eBFa1050', 0,
-                {
-                    gasLimit: 3500000,
-                });
-            console.log(result);
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-    const getPoolDataDataLocal = async (e) => {
-        try {
-            const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(contractAddress,
-                TestInvestment.abi, signer);
-            const result = await contract.getPoolData();
-            console.log(result);
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-
 
     const refund = async (e) => {
         try {
@@ -161,7 +68,7 @@ function HomePage() {
             const accounts = await provider.listAccounts();
             const signer = provider.getSigner();
             const contract = new ethers.Contract(contractAddress,
-                TestInvestment.abi, signer);
+                Pool.abi, signer);
 
             const tx = await contract.finishInvestment(1, {
                 from: accounts[0],
@@ -181,31 +88,9 @@ function HomePage() {
                 Invest
             </button>
         </p>
-        <p>
-            <button onClick={investLocal}>
-                Invest Local
-            </button>
-        </p>
-        <p>
-            <button onClick={getMyInvestmentsLocal}>
-                Get My Investments Local
-            </button>
-        </p>
-        <p>
-            <button onClick={getInvestDataLocal}>
-                Get Invest Data Local (0)
-            </button>
-        </p>
-        <p>
-            <button onClick={rebalanceLocal}>
-                Rebalance Local
-            </button>
-        </p>
-        <p>
-            <button onClick={getPoolDataDataLocal}>
-                Get Pool Data Local
-            </button>
-        </p>
+     
+      
+
         <p>
             <button onClick={refund}>
                 Refund
