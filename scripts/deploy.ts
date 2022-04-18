c1ba70c5bf785056a903026c67680382014098c5
ahmad mkh <apolonsoft@gmail.com>
2022-04-18 15:48:06 +0430

    deployed new pool

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -27,14 +27,14 @@ async function main() {
   // const poolTokens = ['0xa6fa4fb5f76172d178d61b04b0ecd319c5d1c0aa', '0x326C977E6efc84E512bB9C30f76E30c160eD06FB', '0x001b3b4d0f3714ca98ba10f6042daebf0b1b7b6f'];
   // AAVE, CRV, wBTC, UNI, DAI
   const poolTokens = [
-    "0xd6df932a45c0f255f85145f286ea0b292b21c90b",
-    "0x172370d5cd63279efa6d502dab29171933a610af",
-    // "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
-    // "0xb33eaad8d922b1083446dc23f610c2567fb5180f",
-    //  "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
-    //  "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+    "0xd6df932a45c0f255f85145f286ea0b292b21c90b", // AAVE
+    // "0x172370d5cd63279efa6d502dab29171933a610af", // CRV
+    // "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6", // WBTC
+    "0xb33eaad8d922b1083446dc23f610c2567fb5180f", // UNI
+    "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063", // DAI
+    "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619", // WETH
   ];
-  const poolTokenPercentage = [60, 40];
+  const poolTokenPercentage = [25, 25, 25, 25];
   // We get the contract to deploy
   const Pool = await ethers.getContractFactory("Pool");
   const pool = await Pool.deploy(
@@ -50,14 +50,14 @@ async function main() {
   console.log("Init balances");
   // const data = await pool.getPoolData();
   // console.log("Pool data: ", data);
-
+  /*
   const result = await signer.sendTransaction({
     to: pool.address,
     value: ethers.utils.parseEther("0.01"),
     gasLimit: 6000000,
   });
   console.log("res:", (await result.wait()).transactionHash);
-
+ */
   // console.log('NEW data: ', await pool.getPoolData())
 }
 
