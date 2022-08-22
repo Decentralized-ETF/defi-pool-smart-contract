a702aeaf9f6e47d9e0bfcfb23154f1f239a04a7d
Dmitry Sonder <newoldwill@gmail.com>
2022-08-22 11:30:03 +0800

    deployment fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,102 +1,107 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv'
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig, task } from 'hardhat/config'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
 import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "hardhat-contract-sizer";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "@nomiclabs/hardhat-ethers";
-import "@openzeppelin/hardhat-upgrades";
+import 'hardhat-contract-sizer'
+import '@typechain/hardhat'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+import '@nomiclabs/hardhat-ethers'
+import '@openzeppelin/hardhat-upgrades'
 
-dotenv.config();
+dotenv.config()
 
-const { pk } = require("./secrets.json");
+const { pk } = require('./secrets.json')
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
+
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY
+const BSCSCAN_KEY = process.env.BSCSCAN_KEY
+const OPTIMISTICSCAN_KEY = process.env.OPTIMISTICSCAN_KEY
+const HECOSCAN_KEY = process.env.HECOSCAN_KEY
+const FTMSCAN_KEY = process.env.FTMSCAN_KEY
+const ARBISCAN_KEY = process.env.ARBISCAN_KEY
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY
+const AVALANCHE_KEY = process.env.AVALANCHE_KEY
 
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.15",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          }
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.15',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+            {
+                version: '0.5.16',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+            {
+                version: '0.6.6',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+    },
+    networks: {
+        hardhat: {
+            allowUnlimitedContractSize: true,
         },
-      },
-      {
-        version: "0.5.16",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          }
+        mumbai: {
+            url: 'https://rpc-mumbai.maticvigil.com/',
+            // network_id: 80001,
+            // confirmations: 2,
+            // timeoutBlocks: 200,
+            // networkCheckTimeout: 50000000,
+            accounts: [pk],
+            // skipDryRun: true,
+            gas: 6000000,
+            gasPrice: 18000000000,
         },
-      },
-      {
-        version: "0.6.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          }
+        polygon: {
+            url: 'https://polygon-rpc.com/',
+            // network_id: 80001,
+            // confirmations: 2,
+            // timeoutBlocks: 200,
+            // networkCheckTimeout: 50000000,
+            accounts: [pk],
+            // skipDryRun: true,
+            gas: 6000000,
+            gasPrice: 60000000000,
         },
-      },
-    ],
-  },
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true
+        ropsten: {
+            url: process.env.ROPSTEN_URL || '',
+            accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        },
     },
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com/",
-      // network_id: 80001,
-      // confirmations: 2,
-      // timeoutBlocks: 200,
-      // networkCheckTimeout: 50000000,
-      accounts: [
-        pk
-      ],
-      // skipDryRun: true,
-      gas: 6000000,
-      gasPrice: 18000000000,
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: 'USD',
     },
-    polygon: {
-      url: "https://polygon-rpc.com/",
-      // network_id: 80001,
-      // confirmations: 2,
-      // timeoutBlocks: 200,
-      // networkCheckTimeout: 50000000,
-      accounts: [
-        pk
-      ],
-      // skipDryRun: true,
-      gas: 6000000,
-      gasPrice: 60000000000,
+    etherscan: {
+        apiKey: POLYGONSCAN_KEY
     },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-};
+}
 
-export default config;
+export default config
--- scripts/debug.ts
+++ scripts/debug.ts
@@ -1,14 +1,17 @@
 import { loadPools, loadCore } from './utils'
 import { Factory } from '../typechain'
 import hre from 'hardhat'
+import {Deployer} from "./classes/Deployer";
 
 async function debug() {
     const core = await loadCore()
-    const factory = (await hre.ethers.getContractAt('Factory', core.factory)) as Factory
-    const pool = await factory.pools(0)
-    const poolStorage = await factory.poolStorages(0)
-    console.log(pool, 'pool')
-    console.log(poolStorage, 'poolStorage')
+    const deployer = new Deployer();
+    await deployer.verify(core.factory, )
+    // const factory = (await hre.ethers.getContractAt('Factory', core.factory)) as Factory
+    // const pool = await factory.pools(0)
+    // const poolStorage = await factory.poolStorages(0)
+    // console.log(pool, 'pool')
+    // console.log(poolStorage, 'poolStorage')
 }
 
 if (require.main === module) {
